a83ec649e8b39b6f02c858d9428118d297e501eb
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2022-11-18 16:14:20 +0100

    Feature Cart

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart.service';
+import { Cart } from '../shared/models/Cart';
+import { CartItems } from '../shared/models/CartItems';
 
 @Component({
   selector: 'app-cart-page',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-page.component.css']
 })
 export class CartPageComponent implements OnInit {
-
-  constructor() { }
+  cart!: Cart;
+  constructor(private cartService: CartService) {
+    this.setCart();
+  }
 
   ngOnInit(): void {
   }
+  setCart() {
+    this.cart = this.cartService.getCart();
+  }
 
+  removeFromCart(cartItme: CartItems) {
+    this.cartService.removeFromCart(cartItme.food.id);
+    this.setCart();
+  }
+
+  changeQuantity(cartItme: CartItems, quantityInString: string) {
+    const quantity = parseInt(quantityInString);
+    this.cartService.changeQuantity(cartItme.food.id, quantity);
+    this.setCart();
+  }
 }
--- src/app/foodpage/foodpage.component.ts
+++ src/app/foodpage/foodpage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartService } from '../services/cart.service';
 import { FoodService } from '../services/food/food.service';
 import { Foods } from '../shared/models/foods';
 
@@ -12,7 +13,7 @@ export class FoodpageComponent implements OnInit {
   food!: Foods;
 
   constructor(private activatedRoute: ActivatedRoute,
-    private foodService: FoodService) {
+    private foodService: FoodService, private cartService: CartService, private router: Router) {
     activatedRoute.params.subscribe((params) => {
       if (params['id'])
         this.food = foodService.getFoodById(params['id'])
@@ -22,4 +23,8 @@ export class FoodpageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  addToCart() {
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page')
+  }
 }
